ec08ab06b405f1dbbeea22bc211cd0a886eb1298
yunkim <1106qwe@hanmail.net>
2023-06-12 20:43:40 +0900

    2차

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,15 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public CustomWebApplicationServer(int port) {
         this.port = port;
@@ -28,37 +33,7 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null){
                 logger.info("[CustomWebApplicationServer] client connected.");
 
-                //Step1 - 사용자 요청을 메인 Thread가 처리
-
-                //InputStream → InputStreamReader 로 변경을 통해 라인별로 읽을 수 있도록 한다.
-                try (InputStream in = clientSocket.getInputStream();
-                     OutputStream out = clientSocket.getOutputStream()){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    String line;
-                    while ((line = br.readLine()) != ""){
-                        if(line != null) {
-                            System.out.println(line);
-                        }
-                    }
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+                new Thread(new ClientRequestHandler(clientSocket)).start();
 
             }//while
         }//try
